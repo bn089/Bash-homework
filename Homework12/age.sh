be87f7c618135c0d1d1b9bb175dcc3d63c6b1977
bn089 <nurkamilbubusara@gmail.com>
2025-03-02 02:02:09 +0000

    folder Homework12 added

--- Homework12/age.sh
+++ Homework12/age.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 read -p "Enter year of birth: " year 
-
 if [ $year -lt 2025 ] 
-then
-echo -e You are $((2025-year)) years old 
-fi
+	then
+	echo -e You are $((2025-year)) years old 
 
+fi
 
 
 read -p "Enter year of birth: " year 
-
 if [ $year -lt 2020 ]
-then
+	then	
 	echo -e You were $((2020-year)) years old in 2020
 fi
 
+
 read -p "Enter year of birth: " year
 if [ $year -lt 2030 ]
-then
+	then
 	echo -e You will be $((2030-year)) years old in 2030
 fi
 
--- Homework12/steak.sh
+++ Homework12/steak.sh
@@ -3,23 +3,34 @@
 read -p "What is the meat temperature? " temp
 
 if [ $temp -ge 120 ] && [ $temp -le 130 ]
-then 
-echo "Rare" 
+	then 
+		echo "Rare" 
 
+
+	
 elif [ $temp -ge 131 ] && [ $temp -le 140 ]
-then
-	echo "Medium rare" 
+	then
+		echo "Medium rare" 
 
+
+	
 elif [ $temp -ge 141 ] && [ $temp -le 150 ]
 	then
 		echo "Medium"
 
-	elif [ $temp -ge 151 ] && [ $temp -le 160 ]
+
+	
+elif [ $temp -ge 151 ] && [ $temp -le 160 ]
 	then
 		echo "Medium well"
 
-	elif [ $temp -ge 161 ] && [ $temp -le 170 ] 
-		then
-			echo "Well done" 
 
-fi 
+
+elif [ $temp -ge 161 ] && [ $temp -le 170 ] 
+	then
+		echo "Well done" 
+
+
+	else 
+		echo "Select the right choice" 
+fi
